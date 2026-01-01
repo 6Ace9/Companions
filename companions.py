d89e3b1a0657aed84141075a3eddcdb0a4c2e3f6
6Ace9 <sueodfbdjes@protonmail.com>
2025-12-31 17:15:17 -0800

    Update companions.py

--- companions.py
+++ companions.py
@@ -100,7 +100,7 @@ st.markdown("""
             onclick="alert('You chose the wise Owl companion 🦉')">
     </button>
     <button class="ring-button" id="koi-ring" 
-            style="top: 45%; left: 30%;"
+            style="top: 45%; left: 500%;"
             onclick="alert('You chose the compassionate Koi companion 🐟')">
     </button>
     <button class="ring-button" id="fox-ring" 
