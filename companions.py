0ce80ce1e91020870b46d69070d4d3a040005000
6Ace9 <sueodfbdjes@protonmail.com>
2025-12-31 18:30:08 -0800

    Update companions.py

--- companions.py
+++ companions.py
@@ -102,7 +102,7 @@ css = f"""
 
     /* Fox ring - Orange (already had strong hover) */
     #fox-ring {{
-        top: 64%;
+        top: 63%;
         left: 70%;
         transform: translate(-50%, -50%);
         border-color: #fb923c;
